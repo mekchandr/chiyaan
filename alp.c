dcd58387a5b560983415abefe21ea9325d7758ae
mekchandr <35653274+mekchandr@users.noreply.github.com>
2018-02-01 16:57:59 +0530

    Create alp.c

---
+++ alp.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+printf("type the letter");
+scanf("%c",&c);
+if(c>='a'&&c<='z')
+printf("alphabet");
+else
+    printf("not alphabet");
+}
